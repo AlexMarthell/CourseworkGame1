820919a88ef03b3f7377b2e70fb5a5bdd7a2a902
Dunaev Oleg <puc9l0410@gmail.com>
2021-11-19 22:23:26 +0300

    patch 000000002
    add first zombie
    add zombie script

---
+++ ZombieGame/Assets/Scripts/Zombie1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie1 : MonoBehaviour
+{
+    public int health;
+    public float speed;
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
